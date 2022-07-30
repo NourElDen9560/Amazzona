35241b7d301f31f7bd0e954330c79b7c4245f2eb
hema <hema.1998.man@gmail.com>
2022-07-30 15:13:27 +0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ const userRoutes = require("../routes/user.routes")
 const productRoutes = require("../routes/product.routes")
 //routes
 app.use("/api/user",userRoutes)
-app.use("/api/post",productRoutes)
+app.use("/api/product",productRoutes)
 // 404 error route
 app.all("*", (req,res)=>{
     res.status(404).send({
