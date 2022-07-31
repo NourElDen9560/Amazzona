7ebaad65bf1c087628bfc06b5b5fa478844ea4bc
NourElDen9560 <enour9560@gmail.com>
2022-07-31 18:50:26 +0200

    update product controller and Test Api

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,54 +1,26 @@
-const productSchema = require("../database/models/product.model")
-const res_gen = require("../app/helper/methods").res_gen    /* ### LOOK AT NOTES IN END OT FILE ### */  
-class Product {
+const router = require("express").Router()
+const product = require("../app/controller/product.controller")
+const {auth , authAdmin} = require("../middleware/auth")
+const upload = require("../middleware/files.middleware")
 
-    static add_prodcut = async (req, res) =>{ 
-        try{res_gen(res, 201, await new productSchema(req.body).save(), "Product added successfully")}
-        catch (e) { res_gen(res, 501, e.message, "Cannot add this product") }}
+// release 1 : all user can control 
+//router.get("/add_prodcut",(req,res)=>{res.render("h")}) 
 
-    static list_all_products = async (req, res) => {  
-        try{ res_gen(res, 202, await productSchema.find() , "list all Products") }
-        catch (e) {res_gen(res, 503, e.message, "Cannot list this Product")}}
+router.post("/add_prodcut",product.add_prodcut) 
+router.patch("/edit_product" ,product.edit_product)   
+router.delete("/delete_product" ,product.delete_product)  
+///////////////////////////////////////////////////////////////////////////
+router.post("/list_all_products", product.list_all_products) 
+router.post("/list_single_product",product.list_single_product)
+///////////////////////////////////////////////////////////////////////////
+router.post("/add_comment/" ,product.add_comment) //  
+router.patch("/edit_comment/"  ,product.edit_comment)
+router.delete("/delete_comment/"  , product.delete_comment)
+///////////////////////////////////////////////////////////////////////////
+router.post("/add_rate/", product.add_rate)
+router.get("/show_rate/", product.show_rate)
+///////////////////////////////////////////////////////////////////////////
+router.post("/sold_counter_add/" , product.sold_counter_add)
+router.get("/sold_counter_show/" , product.sold_counter_show)
 
-    static list_single_product = async (req, res)=>{
-        try{ res_gen(res, 202, await productSchema.findById(req.body.id) , "list single Product") }
-        catch (e) {res_gen(res, 503, e.message, "Cannot list this Product")}}
-
-    static edit_product = async (req, res) => {
-        try{ const product = await productSchema.findById(req.body.id)
-              for(let key in req.body){ if(req.body[key]) {product[key] = req.body[key]}}
-              res_gen(res, 200, await product.save(), "Product edited successfully")}
-        catch(e){res_gen(res, 500, e.message, "Cannot edit this Product")}}
-
-    static delete_product = async (req, res) => {
-        try{ res_gen(res, 200, await productSchema.findByIdAndDelete(req.body.id).save(), "Product deleted successfully")}
-        catch(e) {res_gen(res, 500, e.message, "cann't delete this product")}}
-
-    static add_comment = async (req, res) => {
-        try{ const task = await taskModel.findById(req.body.id)
-             res_gen(res, 200, task.comments.push(req.body.comment).save(), "Comment added successfully")}
-        catch(e){res_gen(res, 500, e.message, "Cannot add comment")}}
-        
-    static delete_comment = async (req, res) => {
-        try{ const task = await taskModel.findById(req.body.id)
-             const index = task.comments.findIndex(comment => comment._id.toString() == req.body.commentid)
-             res_gen(res, 200, task.comments.splice(index, 1).save(), "comment deleted successfully")}
-        catch(e) {res_gen(res, 500, e.message, "Cannot delete comment")}}
-
-
-    static edit_comment = async (req, res) => {
-        try{
-            const task = await taskModel.findById(req.body.id)
-            const index = task.comments.findIndex(comment => comment._id.toString() == req.body.commentid)
-            task.comments[index] = req.body.comment
-            res_gen(res, 200, await task.save(), "comment edited successfully")}
-        catch (e){res_gen(res, 500, e.message, "cann't edit comment")}}
-
-}module.exports = Product
-
-/*   
-    # Until NOW ALL USER CAN ADD PRODUCTS => THEN ADMIN ONLY CAN ADD IT  
-    # ALL USERS TYPE CAN GET ALL PRODCUTS 
-    # make end user and admin can add comment to task
-    # make end user cand edit commment from task
-*/
+module.exports=router
